38ae477e3c1eb59872e678094f1a66d5b9366a86
Thasso Griebel <thasso.griebel@gmail.com>
2022-01-27 12:53:33 +0100

    Added try catch and more log output

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-const ref = github.context.ref;
-core.info(`Extract version from ${ref}`);
-const segments = ref.split("/");
-let version = segments[2]
-if (version.startsWith("v")) {
-    version = version.substring(1);
+try {
+    const ref = github.context.ref;
+    core.info(`Extract version from ${ref}`);
+    const segments = ref.split("/");
+    let version = segments[2]
+    if (version.startsWith("v")) {
+        version = version.substring(1);
+    }
+    if (ref.startsWith("refs/pull/")) {
+        version = "PR-" + version;
+    }
+    core.info(`Extracted version: '${version}'`)
+    core.setOutput("version", version);
+    core.exportVariable("REF_VERSION", version);
+}catch (error) {
+    core.setFailed(error)
 }
-if (ref.startsWith("refs/pull/")) {
-    version = "PR-" + version;
-}
-core.exportVariable("REF_VERSION", version);
-core.setOutput("version", version);
 
